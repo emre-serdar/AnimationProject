395a905b6112fb42ad8c43c8c5e1da58799ba595
emre-serdar <eserdar1@binghamton.edu>
2021-10-10 16:25:13 -0400

    Problems between kotlin and libgdx are solve finally, Aim Training project is started.

---
+++ AimTraining/desktop/src/edu/binghamton/cs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.binghamton.cs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.binghamton.cs.AimTraining;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AimTraining(), config);
+	}
+}
